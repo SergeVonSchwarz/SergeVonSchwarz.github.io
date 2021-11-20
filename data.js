038d2803c729513c84dd1e26be7e5ed1a37ce28b
Sergio <sergio.glock@gmail.com>
2021-11-20 22:41:17 +0200

    languages and refactoring

--- data.js
+++ data.js
@@ -1,102 +1,206 @@
-const data = [
-  {
-    boxName: "Baskets",
-    items: [
-      {
-        categoryName: "Decorative",
-        categoryItems: [
-          { src: "./images/baskets/b1.jpg" },
-          { src: "./images/baskets/b2.jpg" },
-          { src: "./images/baskets/b6.jpg" },
-          { src: "./images/baskets/b7.jpg" },
-          { src: "./images/baskets/b8.jpg" },
-          { src: "./images/baskets/b16.jpg" },
-        ],
-      },
-      {
-        categoryName: "Simple",
-        categoryItems: [
-          { src: "./images/baskets/b3.jpg" },
-          { src: "./images/baskets/b4.jpg" },
-          { src: "./images/baskets/b14.jpg" },
-          { src: "./images/baskets/b15.jpg" },
-          { src: "./images/baskets/b17.jpg" },
-          { src: "./images/baskets/b12.jpg" },
-          { src: "./images/baskets/b13.jpg" },
-        ],
-      },
-      {
-        categoryName: "Hostess",
-        categoryItems: [
-          { src: "./images/baskets/b5.jpg" },
-          { src: "./images/baskets/b9.jpg" },
-          { src: "./images/baskets/b10.jpg" },
-          { src: "./images/baskets/b11.jpg" },
-          { src: "./images/baskets/b18.jpg" },
-          { src: "./images/baskets/b19.jpg" },
-        ],
-      },
-      {
-        categoryName: "Shelves",
-        categoryItems: [
-          { src: "./images/baskets/bm1.jpg" },
-          { src: "./images/baskets/bm2.jpg" },
-          { src: "./images/baskets/bm3.jpg" },
-          { src: "./images/baskets/bm4.jpg" },
-          { src: "./images/baskets/bm5.jpg" },
-          { src: "./images/baskets/bm6.jpg" },
-          { src: "./images/baskets/bm7.jpg" },
-          { src: "./images/baskets/bm8.jpg" },
-        ],
-      },
-    ],
-  },
-  {
-    boxName: "Rocking chairs",
-    items: [
-      {
-        categoryName: "Opium",
-        categoryItems: [
-          { src: "./images/armchairs/opium/1.jpg" },
-          { src: "./images/armchairs/opium/2.jpg" },
-          { src: "./images/armchairs/opium/3.jpg" },
-          { src: "./images/armchairs/opium/4.jpg" },
-        ],
-      },
-      {
-        categoryName: "Zeppelin",
-        categoryItems: [
-          { src: "./images/armchairs/zeppelin/1.jpg" },
-          { src: "./images/armchairs/zeppelin/2.jpg" },
-          { src: "./images/armchairs/zeppelin/3.jpg" },
-          { src: "./images/armchairs/zeppelin/4.jpg" },
-        ],
-      },
-      {
-        categoryName: "Demiurge",
-        categoryItems: [
-          { src: "./images/armchairs/demiurge/1.jpg" },
-          { src: "./images/armchairs/demiurge/2.jpg" },
-          { src: "./images/armchairs/demiurge/3.jpg" },
-          { src: "./images/armchairs/demiurge/4.jpg" },
-        ],
-      },
-      {
-        categoryName: "Colonel",
-        categoryItems: [
-          { src: "./images/armchairs/colonel/1.jpg" },
-          { src: "./images/armchairs/colonel/2.jpg" },
-          { src: "./images/armchairs/colonel/3.jpg" },
-          { src: "./images/armchairs/colonel/4.jpg" },
-        ],
-      },
-      {
-        categoryName: "Demerzel",
-        categoryItems: [
-          { src: "./images/armchairs/demerzel/1.jpg" },
-          { src: "./images/armchairs/demerzel/2.jpg" },
-        ],
-      },
-    ],
-  },
-]
+const langData = {
+  en: [
+    {
+      boxName: "Baskets",
+      items: [
+        {
+          categoryName: "Decorative",
+          categoryItems: [
+            { src: "./images/baskets/b1.jpg" },
+            { src: "./images/baskets/b2.jpg" },
+            { src: "./images/baskets/b6.jpg" },
+            { src: "./images/baskets/b7.jpg" },
+            { src: "./images/baskets/b8.jpg" },
+            { src: "./images/baskets/b16.jpg" },
+          ],
+        },
+        {
+          categoryName: "Simple",
+          categoryItems: [
+            { src: "./images/baskets/b3.jpg" },
+            { src: "./images/baskets/b4.jpg" },
+            { src: "./images/baskets/b14.jpg" },
+            { src: "./images/baskets/b15.jpg" },
+            { src: "./images/baskets/b17.jpg" },
+            { src: "./images/baskets/b12.jpg" },
+            { src: "./images/baskets/b13.jpg" },
+          ],
+        },
+        {
+          categoryName: "Hostess",
+          categoryItems: [
+            { src: "./images/baskets/b5.jpg" },
+            { src: "./images/baskets/b9.jpg" },
+            { src: "./images/baskets/b10.jpg" },
+            { src: "./images/baskets/b11.jpg" },
+            { src: "./images/baskets/b18.jpg" },
+            { src: "./images/baskets/b19.jpg" },
+          ],
+        },
+        {
+          categoryName: "Shelves",
+          categoryItems: [
+            { src: "./images/baskets/bm1.jpg" },
+            { src: "./images/baskets/bm2.jpg" },
+            { src: "./images/baskets/bm3.jpg" },
+            { src: "./images/baskets/bm4.jpg" },
+            { src: "./images/baskets/bm5.jpg" },
+            { src: "./images/baskets/bm6.jpg" },
+            { src: "./images/baskets/bm7.jpg" },
+            { src: "./images/baskets/bm8.jpg" },
+          ],
+        },
+      ],
+    },
+    {
+      boxName: "Rocking chairs",
+      items: [
+        {
+          categoryName: "Opium",
+          categoryItems: [
+            { src: "./images/armchairs/opium/1.jpg" },
+            { src: "./images/armchairs/opium/2.jpg" },
+            { src: "./images/armchairs/opium/3.jpg" },
+            { src: "./images/armchairs/opium/4.jpg" },
+          ],
+        },
+        {
+          categoryName: "Zeppelin",
+          categoryItems: [
+            { src: "./images/armchairs/zeppelin/1.jpg" },
+            { src: "./images/armchairs/zeppelin/2.jpg" },
+            { src: "./images/armchairs/zeppelin/3.jpg" },
+            { src: "./images/armchairs/zeppelin/4.jpg" },
+          ],
+        },
+        {
+          categoryName: "Demiurge",
+          categoryItems: [
+            { src: "./images/armchairs/demiurge/1.jpg" },
+            { src: "./images/armchairs/demiurge/2.jpg" },
+            { src: "./images/armchairs/demiurge/3.jpg" },
+            { src: "./images/armchairs/demiurge/4.jpg" },
+          ],
+        },
+        {
+          categoryName: "Colonel",
+          categoryItems: [
+            { src: "./images/armchairs/colonel/1.jpg" },
+            { src: "./images/armchairs/colonel/2.jpg" },
+            { src: "./images/armchairs/colonel/3.jpg" },
+            { src: "./images/armchairs/colonel/4.jpg" },
+          ],
+        },
+        {
+          categoryName: "Demerzel",
+          categoryItems: [
+            { src: "./images/armchairs/demerzel/1.jpg" },
+            { src: "./images/armchairs/demerzel/2.jpg" },
+          ],
+        },
+      ],
+    },
+  ],
+  ru: [
+    {
+      boxName: "Корзины",
+      items: [
+        {
+          categoryName: "Декоративные",
+          categoryItems: [
+            { src: "./images/baskets/b1.jpg" },
+            { src: "./images/baskets/b2.jpg" },
+            { src: "./images/baskets/b6.jpg" },
+            { src: "./images/baskets/b7.jpg" },
+            { src: "./images/baskets/b8.jpg" },
+            { src: "./images/baskets/b16.jpg" },
+          ],
+        },
+        {
+          categoryName: "Компактные",
+          categoryItems: [
+            { src: "./images/baskets/b3.jpg" },
+            { src: "./images/baskets/b4.jpg" },
+            { src: "./images/baskets/b14.jpg" },
+            { src: "./images/baskets/b15.jpg" },
+            { src: "./images/baskets/b17.jpg" },
+            { src: "./images/baskets/b12.jpg" },
+            { src: "./images/baskets/b13.jpg" },
+          ],
+        },
+        {
+          categoryName: "Хозяйственные",
+          categoryItems: [
+            { src: "./images/baskets/b5.jpg" },
+            { src: "./images/baskets/b9.jpg" },
+            { src: "./images/baskets/b10.jpg" },
+            { src: "./images/baskets/b11.jpg" },
+            { src: "./images/baskets/b18.jpg" },
+            { src: "./images/baskets/b19.jpg" },
+          ],
+        },
+        {
+          categoryName: "Бельевые",
+          categoryItems: [
+            { src: "./images/baskets/bm1.jpg" },
+            { src: "./images/baskets/bm2.jpg" },
+            { src: "./images/baskets/bm3.jpg" },
+            { src: "./images/baskets/bm4.jpg" },
+            { src: "./images/baskets/bm5.jpg" },
+            { src: "./images/baskets/bm6.jpg" },
+            { src: "./images/baskets/bm7.jpg" },
+            { src: "./images/baskets/bm8.jpg" },
+          ],
+        },
+      ],
+    },
+    {
+      boxName: "Кресла качалки",
+      items: [
+        {
+          categoryName: "Опиум",
+          categoryItems: [
+            { src: "./images/armchairs/opium/1.jpg" },
+            { src: "./images/armchairs/opium/2.jpg" },
+            { src: "./images/armchairs/opium/3.jpg" },
+            { src: "./images/armchairs/opium/4.jpg" },
+          ],
+        },
+        {
+          categoryName: "Дирижабль",
+          categoryItems: [
+            { src: "./images/armchairs/zeppelin/1.jpg" },
+            { src: "./images/armchairs/zeppelin/2.jpg" },
+            { src: "./images/armchairs/zeppelin/3.jpg" },
+            { src: "./images/armchairs/zeppelin/4.jpg" },
+          ],
+        },
+        {
+          categoryName: "Демиург",
+          categoryItems: [
+            { src: "./images/armchairs/demiurge/1.jpg" },
+            { src: "./images/armchairs/demiurge/2.jpg" },
+            { src: "./images/armchairs/demiurge/3.jpg" },
+            { src: "./images/armchairs/demiurge/4.jpg" },
+          ],
+        },
+        {
+          categoryName: "Старый Хер",
+          categoryItems: [
+            { src: "./images/armchairs/colonel/1.jpg" },
+            { src: "./images/armchairs/colonel/2.jpg" },
+            { src: "./images/armchairs/colonel/3.jpg" },
+            { src: "./images/armchairs/colonel/4.jpg" },
+          ],
+        },
+        {
+          categoryName: "Демерзель",
+          categoryItems: [
+            { src: "./images/armchairs/demerzel/1.jpg" },
+            { src: "./images/armchairs/demerzel/2.jpg" },
+          ],
+        },
+      ],
+    },
+  ],
+}
